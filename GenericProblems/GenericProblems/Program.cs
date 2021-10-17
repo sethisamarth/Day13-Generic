09cf9ca2b70e6f4c9e8a73f3701e1b9405094e10
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 23:01:44 +0530

    Print array using generic

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -4,41 +4,23 @@ namespace GenericProblems
 {
     class Program
     {
-        public static void toPrint(int[] inputArray)
+        public static void toPrint<T>(T[] inputArray)
         {
-            foreach (int element in inputArray)
+            foreach (var element in inputArray)
             {
                 Console.WriteLine(element);
             }
             Console.WriteLine("--------------------------------------");
-
-
-        }
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("--------------------------------------");
-        }
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("--------------------------------------");
-
         }
         static void Main(string[] args)
         {
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
             char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
-            Program.toPrint(intArray);
+            Program.toPrint<int>(intArray);
             Program.toPrint(doubleArray);
             Program.toPrint(charArray);
         }
     }
 }
+
