feda186bc5fedd03c4d11934e4fcb9105f72d36d
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 22:55:06 +0530

    Print array of int, double and char

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -4,9 +4,41 @@ namespace GenericProblems
 {
     class Program
     {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------------------------");
+
+
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------------------------");
+        }
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------------------------");
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
         }
     }
 }
